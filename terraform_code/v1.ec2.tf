3f278cb6dd36b9c606086b9c37a4edaafcd600f3
ravitheja <ravithejareddy8008@gmail.com>
2024-03-16 11:18:45 +0530

    terraform commit

---
+++ terraform_code/v1.ec2.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    region = "us-east-1" 
+    
+}
+
+resource "aws_instance" "demo-server"{
+    ami = "ami-080e1f13689e07408"
+    instance_type ="t2.micro"
+    key_name = "dpp"
+}
