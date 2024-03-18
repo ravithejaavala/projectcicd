97f6bf9ff84d8a37bc85d945ea1764196de8dc6d
ravitheja <ravithejareddy8008@gmail.com>
2024-03-18 18:30:52 +0530

    commited

--- terraform_code/v1.ec2.tf
+++ terraform_code/v1.ec2.tf
@@ -37,7 +37,7 @@ resource "aws_security_group" "demo-sg" {
   }
   ingress {
     description      = "Webport port"
-    from_port        = 8080
+    from_port        = 8000
     to_port          = 8000
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
